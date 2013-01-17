626d3d4983513a56a1391bd497eab9ded7bf293c
Timothy Crosley <timothy.crosley@gmail.com>
2013-01-17 14:21:25 -0500

    Html2Shpaml 0.2.0: Improve documentation, add GPL notices to all source files, switch to semantic versioning, support Python3 and WebElements 1.0.0-alpha.1

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from distutils.core import setup
 
 setup(name='Html2Shpaml',
-      version='0.1',
+      version='0.2.0',
       description='A forgiving html to shpaml converter.',
       author='Timothy Crosley',
       author_email='timothy.crosley@gmail.com',
       url='http://www.simpleinnovation.org/',
-      download_url='https://github.com/timothycrosley/Html2Shpaml/blob/master/dist/Html2Shpaml-0.1.tar.gz?raw=true',
+      download_url='https://github.com/timothycrosley/Html2Shpaml/blob/master/dist/Html2Shpaml-0.2.0.tar.gz?raw=true',
       license = "GNU GPLv2",
       scripts=['scripts/html2Shpaml',],
-      requires = ['webelements',],)
+      install_requires=['webelements>=1.0.0-alpha.1',],)
